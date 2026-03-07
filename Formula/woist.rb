832d951e505b85f304ac92741ff807d5a44b36e0
Manu <lemmy@MacBook-Air-von-Manu.local>
2026-03-07 18:06:39 +0100

    update woist to v5.0.0

--- Formula/woist.rb
+++ Formula/woist.rb
@@ -1,17 +1,13 @@
 class Woist < Formula
-  desc "CLI tool to analyze IPs, domains and URLs"
+  desc "CLI tool to analyze IP addresses, domains and URLs"
   homepage "https://github.com/Lemmy8495/woist"
-  url "https://github.com/Lemmy8495/woist/archive/refs/tags/v4.1.0.tar.gz"
-  sha256 "a4e96cf4581fa2a1461b5391cea80d2cbc341741f677a4ad89c2aed3c94f73a6"
+  url "https://github.com/Lemmy8495/woist/archive/refs/tags/v5.0.0.tar.gz"
+  sha256 "b82ed61f9b9e902123495be3db519bdeed1e73dad7a262fc443b1c24fa354941"
   license "MIT"
 
-  depends_on "python@3.12"
+  depends_on "python"
 
   def install
     bin.install "woist"
-  end
-
-  test do
-    system "#{bin}/woist", "--help"
   end
 end
