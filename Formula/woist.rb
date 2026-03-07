18a0a2f83f55c80d6b457bc182b3da74308f2dc0
Manu <lemmy@MacBook-Air-von-Manu.local>
2026-03-07 12:38:15 +0100

    woist 4.0.0

--- Formula/woist.rb
+++ Formula/woist.rb
@@ -1,8 +1,8 @@
 class Woist < Formula
   desc "Show the geographic location of an IP address or URL"
   homepage "https://github.com/Lemmy8495/woist"
-  url "https://github.com/Lemmy8495/woist/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "0c90dce765842c6c953aa19b09414bd16be339606a32a921b0910bf59554060c"
+  url "https://github.com/Lemmy8495/woist/archive/refs/tags/4.0.0.tar.gz"
+  sha256 "6970bf4b5a1625ed45a2228649c5a0c38d5e0cc5b34cc18257095dd7ec91d25a"
   license "MIT"
 
   def install
