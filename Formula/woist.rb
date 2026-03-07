6d8cbd1db3d563afc1e85abcb7030cfd15c33b54
Manu <lemmy@MacBook-Air-von-Manu.local>
2026-03-07 16:38:35 +0100

    fix woist 4.1.0 sha256

--- Formula/woist.rb
+++ Formula/woist.rb
@@ -2,7 +2,7 @@ class Woist < Formula
   desc "CLI tool to analyze IPs, domains and URLs"
   homepage "https://github.com/Lemmy8495/woist"
   url "https://github.com/Lemmy8495/woist/archive/refs/tags/v4.1.0.tar.gz"
-  sha256 "102e2d9d86d3528d75f5b41c169f68d86f72aad1fef594a5c2021f6a0a258a1f"
+  sha256 "a4e96cf4581fa2a1461b5391cea80d2cbc341741f677a4ad89c2aed3c94f73a6"
   license "MIT"
 
   depends_on "python@3.12"
